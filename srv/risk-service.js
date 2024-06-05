7bf1f963ab14babb20106369e92595a826d5ee6e
guilhermeq-dev <guilherme_queiroz97@outlook.com>
2024-06-05 13:18:03 -0300

    fix: added new logic

--- srv/risk-service.js
+++ srv/risk-service.js
@@ -1,18 +1,18 @@
-
-const cds = require('@sap/cds')
+const cds = require("@sap/cds");
 
 /**
  * Implementation for Risk Management service defined in ./risk-service.cds
  */
-module.exports = cds.service.impl(async function() {
-    this.after('READ', 'Risks', risksData => {
-        const risks = Array.isArray(risksData) ? risksData : [risksData];
-        risks.forEach(risk => {
-            if (risk.impact >= 100000) {
-                risk.criticality = 1;
-            } else {
-                risk.criticality = 2;
-            }
-        });
+module.exports = cds.service.impl(async function () {
+  this.after("READ", "Risks", (risksData) => {
+    const risks = Array.isArray(risksData) ? risksData : [risksData];
+    risks.forEach((risk) => {
+      console.log(risk);
+      if (risk.impact >= 100000 && risk.prio == 1) {
+        risk.criticality = 1;
+      } else {
+        risk.criticality = 2;
+      }
     });
+  });
 });

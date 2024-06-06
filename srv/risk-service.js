510b02936c572fe27e38f8b7bdb1dc56f846a650
guilhermeq-dev <guilherme_queiroz97@outlook.com>
2024-06-06 17:50:36 -0300

    feat: added hana db config

--- srv/risk-service.js
+++ srv/risk-service.js
@@ -7,12 +7,14 @@ module.exports = cds.service.impl(async function () {
   this.after("READ", "Risks", (risksData) => {
     const risks = Array.isArray(risksData) ? risksData : [risksData];
     risks.forEach((risk) => {
-      console.log(risk);
       if (risk.impact >= 100000 && risk.prio == 1) {
         risk.criticality = 1;
       } else {
         risk.criticality = 2;
       }
+
+      const sImpact = risk.impact;
+      risk.impact = `U$D ${sImpact}`;
     });
   });
 });
